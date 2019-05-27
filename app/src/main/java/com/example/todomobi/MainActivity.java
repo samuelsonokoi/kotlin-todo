f32369ef1826ef20e8422f4d44cff92266807b1a
samuelsonokoi@gmail.com <samuelsonokoi@gmail.com>
2019-05-27 18:22:26 +0100

    added new google sign in activity

--- app/src/main/java/com/example/todomobi/MainActivity.java
+++ app/src/main/java/com/example/todomobi/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.todomobi;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
+
+public class MainActivity extends TutorialActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        addFragment(new Step.Builder()
+                .setTitle("This is header 1")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.img1) // int top drawable
+                .build());
+
+        addFragment(new Step.Builder()
+                .setTitle("This is header 2")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.img2) // int top drawable
+                .build());
+
+        addFragment(new Step.Builder()
+                .setTitle("This is header 3")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.img3) // int top drawable
+                .build());
+    }
+
+    @Override
+    public void finishTutorial() {
+        // Open google sign when done with slides
+    }
+
+    @Override
+    public void currentFragmentPosition(int position) {
+
     }
 }
